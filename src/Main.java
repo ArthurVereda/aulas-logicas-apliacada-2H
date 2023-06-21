914d808d8e250c50d320a2726d46391950730be9
Arthur F. Vereda <arthurvereda18@gmail.com>
2023-06-21 08:49:32 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        // Aula introdutória de Orientação à Objetos
+        // Declaração de objeto
+        Pessoa adao;
+        // Instanciação do objeto
+        adao = new Pessoa();
+        // Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+        // Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        // comportamento
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
+        // Criar novos objetos
+        // Declarar objeto
+        Pessoa rainha;
+        // Instanciar objetos
+        rainha = new Pessoa();
+        // Definir forma do objeto
+        rainha.nome = "Tina";
+        rainha.sobrenome = "Tunner";
+        // Definição do comportamento
+        rainha.falar();
+        rainha.comer();
+
+        // Criar produtos
+        Produto p1 = new Produto(1300.00);
+        Produto p2 = new Produto();
+        p2.preco = 233.00;
+        Produto p3;
+        p3 = new Produto("Placa Vídeo", 1300.00);
+        p3 = new Produto("Placa Mãe", 2500.00);
+
+
+    }
+}
